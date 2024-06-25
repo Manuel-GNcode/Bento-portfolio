fca1788132279691c8e7442b6277ab360456a89c
GNcode <gngalarga027@gmail.com>
2024-06-24 20:23:27 -0500

    Arreglo de navegación entre páginas, al hacer scroll en una sección y cambiar a otra esta no volvia al inicio conservaba el scroll inicial.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import WorkApp from "../projects/WorksApp";
 import CertificationtsApp from "../certifications/CertificationsApp";
 import ContactApp from "../contact/ContactApp";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from "./components/components";
 
 function App() {
     return (
         <Router>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" exact element={<HomeApp />} />
                 <Route path="/work" element={<WorkApp />} />
