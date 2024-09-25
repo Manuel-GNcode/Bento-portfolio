ae11e6b74d195dda3ad6d2244e05e1e273490421
GNcode <gngalarga027@gmail.com>
2024-09-25 13:43:03 -0500

    Añadí un nuevo proyecto al carrusel de la página principal.

--- home/components/marquee.jsx
+++ home/components/marquee.jsx
@@ -1,6 +1,9 @@
 export const Marquee = ()=>{
     return (
         <div className="wrapper">
+            <span style={{"--i": 0}}>
+                <img alt="Tic Tac Toe game" src="/ticTacToe-second.jpg"></img>
+            </span>
             <span style={{"--i": 1}}>
                 <img alt="2D portfolio using Kaboon.js" src="/2DPortfolio-one.jpg"></img>
             </span>
@@ -8,10 +11,10 @@ export const Marquee = ()=>{
                 <img alt="Lau Illustration" src="/cute-portfolio-1.webp"></img>
             </span>
             <span style={{"--i": 3}}>
-                <img alt="Arkanoid game" src="/arkanoid-1.webp"></img>
+                <img alt="Color Balance" src="/color-balance-1.webp"></img>
             </span>
             <span style={{"--i": 4}}>
-                <img alt="Tic Tac Toe game" src="/ticTacToe-second.jpg"></img>
+                <img alt="Arkanoid game" src="/arkanoid-1.webp"></img>
             </span>
         </div>
     );
